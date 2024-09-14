ed897df9b866ae5289c973d56f2d797b33639b6e
Tauan Silva <tauan.silva@sinqia.com.br>
2024-09-14 13:59:31 -0300

    ajustes

--- .github/workflows/clair-check.sh
+++ .github/workflows/clair-check.sh
@@ -7,13 +7,15 @@ COUNTER=1
 MAX=360
 
 while true; do
-    if docker logs "${CONTAINER}" 2>&1 | grep "starting background updates" >&/dev/null
+    if docker logs "${CONTAINER}" 2>&1 | grep "update finished" >&/dev/null
     then
+        echo "ESTOU AQUI 'update finished'." >&2
         break
     fi
 
     if docker logs "${CONTAINER}" 2>&1 | grep "ERROR" >&/dev/null
     then
+        echo "ESTOU AQUI 'ERROR'." >&2
         docker logs -n 25 "${CONTAINER}"
         echo "Error during update." >&2
         exit 1
