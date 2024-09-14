8b4751f99eaf4155db19a6cb119d32ea1c4ebee4
Tauan Silva <tauan.silva@sinqia.com.br>
2024-09-14 19:51:18 -0300

    teste

--- .github/workflows/clair-check.sh
+++ .github/workflows/clair-check.sh
@@ -5,41 +5,45 @@ set -ex
 CONTAINER="clair"
 COUNTER=1
 MAX=360
-ITERATION_COUNT=0
-LOG_COUNT=2
+LOG1=""
+LOG2=""
+LOG3=""
 
 while true; do
     # Verifica se a atualização foi concluída
     if docker logs "${CONTAINER}" 2>&1 | grep "update finished" >&/dev/null
     then
-        echo "update finished." >&2
+        echo "ESTOU AQUI 'update finished'." >&2
         break
     fi
 
     # Verifica se há algum erro
     if docker logs "${CONTAINER}" 2>&1 | grep "ERROR" >&/dev/null
     then
+        echo "ESTOU AQUI 'ERROR'." >&2
         docker logs -n 25 "${CONTAINER}"
         echo "Error during update." >&2
         exit 1
     fi
 
-    # Obtém o último log e verifica se contém apenas 'updater service started'
-    LAST_LOG=$(docker logs -n 1 "${CONTAINER}")
-    if echo "$LAST_LOG" | grep -q "updater service started"; then
-        ((ITERATION_COUNT++))
-    else
-        ITERATION_COUNT=0  # Reseta o contador se encontrar um log diferente
-    fi
+    # Captura o último log
+    CURRENT_LOG=$(docker logs -n 1 "${CONTAINER}")
 
-    # Se os últimos 3 logs forem 'updater service started', interrompe o loop
-    if [ "$ITERATION_COUNT" -ge "$LOG_COUNT" ]; then
-        echo "Os últimos 3 logs contêm apenas 'updater service started'. Interrompendo o loop." >&2
+    # Shift logs: move o log mais recente para frente e armazena o novo log
+    LOG3="$LOG2"
+    LOG2="$LOG1"
+    LOG1="$CURRENT_LOG"
+
+
+    # Verifica se as últimas 3 iterações tiveram a mesma mensagem de log
+    if [ "$LOG1" == "$LOG2" ] && [ "$LOG2" == "$LOG3" ] && [ -n "$LOG1" ]; then
+        echo "As últimas 3 iterações têm a mesma mensagem de log. Interrompendo o loop." >&2
         break
     fi
 
-    # Exibe o último log
-    echo "$LAST_LOG"
+    docker logs -n 1 "${CONTAINER}"
+
+    # Aguarda 10 segundos antes da próxima iteração
     sleep 10
     ((COUNTER++))
 
